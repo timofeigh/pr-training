5f12065074ea0ba52cb123e458018213b27ca99f
timofeigh <timofeigh@mail.ru>
2026-04-16 18:52:02 +0500

    Изменен метод get_total_price в main

--- src/models/product.py
+++ src/models/product.py
@@ -26,7 +26,7 @@ class Product:
         return self.price < other.price
 
     def get_total_price(self):
-        return self.price * self.quantity
+        return self.price * self.quantity #aaaa
     
     def sell(self, amount):
         if self.quantity < amount:
